880b6019cc6cba7771b277b1c479ef6bc16a5022
Martina Scibilia <martina.scibilia@outlook.com>
2025-10-03 18:13:57 +0200

    Add links to Footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,15 +3,19 @@ export default function Footer() {
     <div className="Footer">
       <p>
         This project was coded by{" "}
-        <a href="#" target="_blank" rel="noreferrer">
+        <a href="https://github.com/masci-hub" target="_blank" rel="noreferrer">
           Martina Scibilia
         </a>
         , versioned on{" "}
-        <a href="#" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/masci-hub/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
           GitHub
         </a>
         , and hosted on{" "}
-        <a href="#" target="_blank" rel="noreferrer">
+        <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
           Netlify
         </a>
         .
