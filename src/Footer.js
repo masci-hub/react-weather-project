14cd4d7844384882165c1d2ba517c67f98f69df5
Martina Scibilia <martina.scibilia@outlook.com>
2025-10-03 18:02:20 +0200

    Create React app and add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This project was coded by{" "}
+        <a href="#" target="_blank" rel="noreferrer">
+          Martina Scibilia
+        </a>
+        , versioned on{" "}
+        <a href="#" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+        , and hosted on{" "}
+        <a href="#" target="_blank" rel="noreferrer">
+          Netlify
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
